94629d4231bf9cd195c189db06e6f9baa9f1570e
bayapucg <vasudevareddy549@gmail.com>
2018-05-23 10:14:37 +0530

    commit
    changes

--- application/views/seller/layouts/_selleradmin.php
+++ application/views/seller/layouts/_selleradmin.php
@@ -1,7 +1,7 @@
 
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Shofus</title>
+<title>Knc seller</title>
 <link rel="icon" href="<?php echo base_url(); ?>assets/seller_admin/images/fav.ico" type="image/x-icon" />
 <!-- jquery-ui css -->
    <link href="<?php echo base_url(); ?>assets/seller/plugins/jquery-ui-1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
